d195126e68e958a26ec7fa97bc9d5231307c56f3
Macarena <maquibarrios@gmail.com>
2023-05-10 14:04:57 -0300

    Lectura de valores input, calculo de valores (output)

---
+++ script.js
@@ -0,0 +1,28 @@
+/* Para las barras deslizadoras "Tip" y "No. of people" */
+const sliders = document.querySelectorAll("input[type='range']");
+//console.log(sliders);
+
+sliders.forEach(function (slider) {
+    slider.addEventListener("input", calculateTip);
+});
+
+const billInput = document.getElementById("bill");
+billInput.addEventListener("change", calculateTip);
+//console.log(billInput);
+
+/* Obtener los valores input */
+function calculateTip() {
+    let bill = parseFloat(billInput.value);
+    let tipPercent = document.getElementById("tip").value;
+    let noOfPeople = document.getElementById("no-of-people").value;
+    //console.log(bill, tipPercent, noOfPeople);
+    billInput.value = bill.toFixed(2);
+
+    /* Obtener los valores output */
+    let totalTip = parseFloat((bill * (tipPercent / 100)).toFixed(2));
+    let total = parseFloat((bill + totalTip).toFixed(2));
+    let tipPerPerson = (totalTip / noOfPeople).toFixed(2);
+    let totalPerPerson = (total / noOfPeople).toFixed(2);
+    //console.log(noOfPeople, totalTip, total, tipPerPerson, totalPerPerson);
+}
+
